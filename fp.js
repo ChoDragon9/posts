e3354bfad21a812f4956e3d9358683775380073c
Yongku cho <04whdydrn30@gmail.com>
2018-05-10 00:43:45 +0900

    Update fp.js

--- fp.js
+++ fp.js
@@ -34,9 +34,51 @@ const under30 = u => u.age < 30
 
 ages(filter(users, under30))
 
-// findBy
+// find, bmatch
 const find = (list, predicate) => {
   for (let item of list) {
     if (predicate(item)) return item
   }
 }
+
+const bmatch = (key, val) => obj => obj[key] === val
+
+find(users, bmatch('id', 3))
+
+// findIndex
+const findIndex = (list, predicate) => {
+  for (let i = 0, len = list.length; i < len; i++) {
+    if (predicate(list[i])) return i
+  }
+  return -1
+}
+
+// identity, falsy, truthly
+const identity = v => v // identity(10)
+const falsy = v => !v
+const truthly = v => !!v
+
+// some, every
+const some = list => truthly(find(list, identity))
+const every = list => filter(list, identity).length === list.length
+
+// not, beq : 연산자 대신 함수로
+const not = v => !v
+const beq = a => b => a === b
+
+const every = list => beq(-1)(findIndex(list, not))
+
+// compose
+const compose = (...fns) => {
+  const start = fns.length - 1
+  return (...args) => {
+    const i = start
+    let result = fns[start].apply(null, args)
+    while (i--) result = fns[i].call(null, result)
+    return result
+  }
+}
+
+const positive = list => find(list, identity)
+// const some = list => truthly(find(list, identity))
+const some = compose(truthly, positive)
