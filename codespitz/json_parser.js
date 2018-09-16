4404f25024c9d924c2c3a30099f39d249f849d1e
peter-cho <peter.cho@kakaocorp.com>
2018-09-16 16:51:18 +0900

    add

--- codespitz/json_parser.js
+++ codespitz/json_parser.js
@@ -17,20 +17,60 @@ const parser = input => {
   let curr = result
   let i = 1, j = input.length - 1
   let key
+  let back
   while (i < j) {
     const cursor = i
     if (input[cursor] === `"`) {
       const idx = input.indexOf(`"`, cursor + 1)
       const str = input.substring(cursor + 1, idx)
-      if (key) {
-        curr[key] = str
-        key = null
+      if (Array.isArray(curr)) {
+        curr.push(str)
       } else {
-        key = str
+        if (key) {
+          curr[key] = str
+          key = null
+        } else {
+          key = str
+        }
+      }
+      i = idx + 1
+    } else {
+      let idx = cursor
+      if (input[cursor] === `}`) {
+        curr = back
+        key = null
+      } else if (input[cursor] === `{`) {
+        const newVal = {}
+        curr[key] = newVal
+        key = null
+        back = curr
+        curr = newVal
+      } else if (input[cursor] === `]`) {
+        curr = back
+        key = null
+      } else if (input[cursor] === `[`) {
+        const newVal = []
+        curr[key] = newVal
+        key = null
+        back = curr
+        curr = newVal
+      } else {
+        if (input[cursor] === '-' || parseInt(input[cursor]) > -1) {
+          const commaIdx = input.indexOf(`,`, cursor + 1)
+          let num
+          if (commaIdx > -1) {
+            num = parseInt(input.substring(cursor, commaIdx).trim())
+            idx = commaIdx + 1
+          } else {
+            num = parseInt(input.substring(cursor, j).trim())
+            idx = j
+          }
+          curr[key] = num
+          key = null
+        }
       }
       i = idx + 1
     }
-    i++
   }
   return result
 }
--- codespitz/spec/json_parser.spec.js
+++ codespitz/spec/json_parser.spec.js
@@ -4,14 +4,62 @@ const {parser} = require('../json_parser')
 describe('json_parser', () => {
   it('parser', () => {
     // Given
-    const html = '{"key": "value"}'
+    const html = '{"key1": "value1", "key2": "value2"}'
 
     // When
     const result = parser(html)
 
     // Then
     expect(result).toEqual({
-      key: "value"
+      key1: "value1",
+      key2: "value2"
+    })
+  })
+  it('parser - Object', () => {
+    // Given
+    const html = '{"key1": {"key1_1":"value1_1"}, "key2": {"key2_1":"value2_1", "key2_2":"value2_2"}}'
+
+    // When
+    const result = parser(html)
+
+    // Then
+    expect(result).toEqual({
+      key1: {
+        key1_1: "value1_1"
+      },
+      key2: {
+        key2_1: "value2_1",
+        key2_2: "value2_2"
+      }
+    })
+  })
+  it('parser - Array', () => {
+    // Given
+    const html = '{"key1": {"key1_1":"value1_1"}, "key2": ["value2_1", "value2_2"]}'
+
+    // When
+    const result = parser(html)
+
+    // Then
+    expect(result).toEqual({
+      key1: {
+        key1_1: "value1_1"
+      },
+      key2: ["value2_1", "value2_2"]
+    })
+  })
+  it('parser - Number', () => {
+    // Given
+    const html = '{"key0": 0, "key1": -1, "key2", 12345}'
+
+    // When
+    const result = parser(html)
+
+    // Then
+    expect(result).toEqual({
+      key0: 0,
+      key1: -1,
+      key2: 12345
     })
   })
 })
