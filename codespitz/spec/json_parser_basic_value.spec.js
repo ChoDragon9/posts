7c4eef83ea9645afdff997c1085a0e3449086f3d
peter-cho <peter.cho@kakaocorp.com>
2018-09-18 22:17:36 +0900

    add

--- codespitz/spec/json_parser_basic_value.spec.js
+++ codespitz/spec/json_parser_basic_value.spec.js
@@ -24,7 +24,7 @@ describe('json_parser_basic_value', () => {
     expect(result).toEqual(``)
   })
 
-  it('parser - String - Special Char', () => {
+  it('parser - String - Unicode', () => {
     // Given
     const html = `"value\\"value"`
 
@@ -35,7 +35,18 @@ describe('json_parser_basic_value', () => {
     expect(result).toEqual(`value\\"value`)
   })
 
-  it('parser - String', () => {
+  it('parser - String - Unicode', () => {
+    // Given
+    const html = `"value\\"value"`
+
+    // When
+    const result = parser(html)
+
+    // Then
+    expect(result).toEqual(`value\\"value`)
+  })
+
+  it('parser - String - Unicode', () => {
     // Given
     const html = `"WHITE FROWNING FACE (U+2639)"`
 
@@ -44,6 +55,19 @@ describe('json_parser_basic_value', () => {
 
     // Then
     expect(result).toEqual("WHITE FROWNING FACE (U+2639)")
+  })
+
+  it('parser - String - Unicode', () => {
+    // Given
+    const html = `{"title":"\u041f\u043e\u043b\u0442\u043e\u0440\u0430 \u0417\u0435\u043c\u043b\u0435\u043a\u043e\u043f\u0430" }`
+
+    // When
+    const result = parser(html)
+
+    // Then
+    expect(result).toEqual({
+      title: `\u041f\u043e\u043b\u0442\u043e\u0440\u0430 \u0417\u0435\u043c\u043b\u0435\u043a\u043e\u043f\u0430`
+    })
   })
 
   it('parser - Number', () => {
