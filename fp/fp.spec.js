6faf33083fe3b8b9b218e45d290dd31763a1ea28
peter-cho <peter.cho@kakaocorp.com>
2018-09-20 00:58:45 +0900

    add

--- fp/fp.spec.js
+++ fp/fp.spec.js
@@ -6,7 +6,9 @@ for (let char of '12345') {
 }
 console.log(arr) // => ['1','2','3','4','5']
 
-const arr = reduce([...'12345'], (acc, val) => {
+const arr2 = reduce([...'12345'], (acc, val) => {
   acc.push(val)
   return acc
 }, [])
+
+console.log(arr2)
