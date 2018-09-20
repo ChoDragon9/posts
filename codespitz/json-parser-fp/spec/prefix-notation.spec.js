fd315a453869cb2a6d8f2591963e8ab951a73c86
peter-cho <peter.cho@kakaocorp.com>
2018-09-20 23:07:55 +0900

    add

--- codespitz/json-parser-fp/spec/prefix-notation.spec.js
+++ codespitz/json-parser-fp/spec/prefix-notation.spec.js
@@ -4,51 +4,51 @@ const {prefixNotation} = require('../prefix-notation')
 describe('prefix-notation', () => {
   it('prefixNotation - String', () => {
     // Given
-    const html = "value"
+    const html = `"value"`
 
     // When
     const result = parser(html)
 
     // Then
-    expect(result).toEqual(value)
+    expect(result).toEqual([`value`])
   })
 
   it('prefixNotation - String - Empty', () => {
     // Given
-    const html = ""
+    const html = `""`
 
     // When
     const result = parser(html)
 
     // Then
-    expect(result).toEqual()
+    expect(result).toEqual(``)
   })
 
   it('prefixNotation - String - Unicode', () => {
     // Given
-    const html = "value\\"value"
+    const html = `"value\\"value"`
 
     // When
     const result = parser(html)
 
     // Then
-    expect(result).toEqual(value\\"value)
+    expect(result).toEqual(`value\\"value`)
   })
 
   it('prefixNotation - String - Unicode', () => {
     // Given
-    const html = "value\\"value"
+    const html = `"value\\"value"`
 
     // When
     const result = parser(html)
 
     // Then
-    expect(result).toEqual(value\\"value)
+    expect(result).toEqual(`value\\"value`)
   })
 
   it('prefixNotation - String - Unicode', () => {
     // Given
-    const html = "WHITE FROWNING FACE (U+2639)"
+    const html = `"WHITE FROWNING FACE (U+2639)"`
 
     // When
     const result = parser(html)
@@ -59,27 +59,27 @@ describe('prefix-notation', () => {
 
   it('prefixNotation - String - Unicode', () => {
     // Given
-    const html = {"title":"\u041f\u043e\u043b\u0442\u043e\u0440\u0430 \u0417\u0435\u043c\u043b\u0435\u043a\u043e\u043f\u0430" }
+    const html = `{"title":"\u041f\u043e\u043b\u0442\u043e\u0440\u0430 \u0417\u0435\u043c\u043b\u0435\u043a\u043e\u043f\u0430" }`
 
     // When
     const result = parser(html)
 
     // Then
     expect(result).toEqual({
-      title: \u041f\u043e\u043b\u0442\u043e\u0440\u0430 \u0417\u0435\u043c\u043b\u0435\u043a\u043e\u043f\u0430
+      title: `\u041f\u043e\u043b\u0442\u043e\u0440\u0430 \u0417\u0435\u043c\u043b\u0435\u043a\u043e\u043f\u0430`
     })
   })
 
   it('prefixNotation - Number', () => {
     // Given
-    const html = [
+    const html = `[
       0.4e006,
       0.4e-006,
       0.4e+006,
       4e006,
       4e-006,
       4e+006
-    ]
+    ]`
 
     // When
     const result = parser(html)
@@ -108,7 +108,7 @@ describe('prefix-notation', () => {
 
   it('prefixNotation - Number - Min/Max', () => {
     // Given
-    const html = { "min": -1.0e+28, "max": 1.0e+28 }
+    const html = `{ "min": -1.0e+28, "max": 1.0e+28 }`
 
     // When
     const result = parser(html)
@@ -119,7 +119,7 @@ describe('prefix-notation', () => {
 
   it('prefixNotation - Truthy Boolean', () => {
     // Given
-    const html = true
+    const html = `true`
 
     // When
     const result = parser(html)
@@ -130,7 +130,7 @@ describe('prefix-notation', () => {
 
   it('prefixNotation - Falsy Boolean', () => {
     // Given
-    const html = false
+    const html = `false`
 
     // When
     const result = parser(html)
@@ -141,7 +141,7 @@ describe('prefix-notation', () => {
 
   it('prefixNotation - null', () => {
     // Given
-    const html = null
+    const html = `null`
 
     // When
     const result = parser(html)
