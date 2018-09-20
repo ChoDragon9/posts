38218df30dd71ca8f5ee2ddc63f8517c140b97ba
peter-cho <peter.cho@kakaocorp.com>
2018-09-21 01:30:21 +0900

    add

--- codespitz/json-parser-fp/json-parser.js
+++ codespitz/json-parser-fp/json-parser.js
@@ -12,15 +12,16 @@ const { extractRef } = require('./extract')
 const { jsonToken } = require('./json-token')
 const parser = input => {
   input = trim(input)
-  const pointer = reduce(jsonToken(input), (pointer, token) => {
-    if (isReference(token)) {
-      pointer = extractRef(pointer, token)
-    } else {
-      setValue(pointer, token)
-    }
-    return pointer
-  }, createNode({}))
-  return getValue(pointer)
+  return `${input}`
+  // const pointer = reduce(jsonToken(input), (pointer, token) => {
+  //   if (isReference(token)) {
+  //     pointer = extractRef(pointer, token)
+  //   } else {
+  //     setValue(pointer, token)
+  //   }
+  //   return pointer
+  // }, createNode({}))
+  // return getValue(pointer)
 }
 
 module.exports = { parser }
