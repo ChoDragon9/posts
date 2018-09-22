c257a0a30955bd57ace042250d3e780ba93c1bae
peter-cho <peter.cho@kakaocorp.com>
2018-09-22 18:21:19 +0900

    add

--- codespitz/json-parser-fp3/json-parser.js
+++ codespitz/json-parser-fp3/json-parser.js
@@ -33,13 +33,12 @@ const parser = input => {
   return stack.getValue()
 }
 
-const same = (...val) => ({char}) => val.every(v => v === char)
-const isObject = same(`{`, `}`)
-const isArray = same(`[`, `]`)
-const isString = same(`"`)
+const isObject = ({char: v}) => v === `{` || v === `}`
+const isArray = ({char: v}) => v === `[` || v === `]`
+const isString = ({char: v}) => v === `"`
 const isNumber = ({char: v}) => v === '-' || parseFloat(v) > -1
-const isBoolean = same(`t`, `f`)
-const isNull = same(`n`)
+const isBoolean = ({char: v}) => v === 't' || v === 'f'
+const isNull = ({char: v}) => v === 'n'
 
 const parseObject = ({char, stack}) => {
   char === `{` ? stack.forword({}) : stack.backword()
