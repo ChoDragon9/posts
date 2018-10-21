aa5bc33d51cd6c53b023fa701e07e6ae7adafdfb
peter-cho <peter.cho@kakaocorp.com>
2018-10-21 18:12:40 +0900

    서비스 변수 다이렉트 접근 방식에서 스트림 접근 방식으로 변경

--- my-angular-app/src/app/todo-list.component.ts
+++ my-angular-app/src/app/todo-list.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {TodoListService} from "./todo-list.service";
 
 @Component({
   selector: 'todo-list',
   templateUrl: './todo-list.component.html',
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  private todo;
   constructor(private todoService: TodoListService) {}
+
+  ngOnInit() {
+    this.todoService.todo$.subscribe(todo => this.todo = todo);
+  }
 
   removeItem(item: string): void {
     this.todoService.remove(item);
--- my-angular-app/src/app/todo-list.service.ts
+++ my-angular-app/src/app/todo-list.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class TodoListService {
-  public todo: Set<string> = new Set();
+  private todo: Set<string> = new Set();
+  private todoSubject = new BehaviorSubject(this.todo);
+
+  get todo$() {
+    return this.todoSubject;
+  }
 
   add(item: string) {
     this.todo.add(item);
+    this.todoSubject.next(this.todo);
   }
 
   remove(item: string) {
     this.todo.delete(item);
+    this.todoSubject.next(this.todo);
   }
 }
