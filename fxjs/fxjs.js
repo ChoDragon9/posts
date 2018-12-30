38142c5e57bfa43259df1a82c3d0551ccf4e63ed
peter.cho <peter.cho@kakaocorp.com>
2018-12-30 16:03:44 +0900

    파일 추가

---
+++ fxjs/fxjs.js
@@ -0,0 +1,82 @@
+const curry = f => (a, ..._) => {
+  return _.length ? f(a, ..._) : (..._) => f(a, ..._);
+};
+const go = (...args) => reduce((a, f) => f(a), args);
+const pipe = (f, ...fs) => (...args) => go(f(...args), ...fs);
+const take = curry((limit, iterable) => {
+  let res = [];
+  for (const a of iterable) {
+    res.push(a);
+    if (res.length === limit) return res;
+  }
+  return res;
+});
+const takeAll = (iterable) => take(Infinity, iterable);
+
+const L = {};
+L.range = function *(l) {
+  let i = -1;
+  while (++i < l) {
+    yield i;
+  }
+};
+
+L.map = curry(function *(mapper, iterable) {
+  for (const a of iterable) {
+    yield mapper(a);
+  }
+});
+L.filter = curry(function *(predicate, iterable) {
+  for (const a of iterable) {
+    if (predicate(a)) yield a;
+  }
+});
+L.entries = function *(obj) {
+  for (const k in obj) yield [k, obj[k]];
+};
+
+const map = curry(pipe(L.map, takeAll));
+const filter = curry(pipe(L.filter, takeAll));
+const reduce = curry((reducer, accumulator, iterable) => {
+  if (!iterable) {
+    iterable = accumulator[Symbol.iterator]();
+    accumulator = iterable.next().value;
+  }
+  for (const a of iterable) {
+    accumulator = reducer(accumulator, a);
+  }
+  return accumulator;
+});
+
+const range = l => {
+  let i = -1;
+  let res = [];
+  while (++i < l) {
+    res.push(i);
+  }
+  return res;
+};
+
+const join = curry((seq = '', iterable) =>
+  reduce((a, b) => `${a}${seq}${b}`, iterable));
+
+const find = curry((predicate, iterable) => go(
+  iterable,
+  L.filter(predicate),
+  take(1),
+  ([a]) => a
+));
+
+const queryStr = pipe(
+  L.entries,
+  L.map(([k, v]) => `${k}=${v}`),
+  join('&')
+)
+
+const evaluate = (name, time, f) => {
+  console.time(name);
+  while(time--) f();
+  console.timeEnd(name);
+}
+const add = (a, b) => a + b;
+const log = console.log;
