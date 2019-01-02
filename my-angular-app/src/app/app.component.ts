b458e8c9bc309e2fac3bad9081c2c0cc82e76adf
peter.cho <peter.cho@kakaocorp.com>
2019-01-02 23:20:47 +0900

    Reactive Froms

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,38 +1,22 @@
 import { Component } from '@angular/core';
 import {of, BehaviorSubject, Subject, Observable} from 'rxjs';
 import {map, share, scan, filter, merge, mergeMap, mergeMapTo, withLatestFrom} from 'rxjs/operators';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  todo: string;
-
-  update$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
-  add$: Subject<string> = new Subject<string>();
-  remove$: Subject<string> = new Subject<string>();
-  number$ = this.update$.pipe(
-    scan((acc, numbers: string[]) => numbers, [])
+  nameUpdate$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  name$ = this.nameUpdate$.pipe(
+    scan((acc, numbers: string) => numbers, '')
   );
 
+  name = new FormControl('');
+
   constructor() {
-    this.add$.pipe(
-      withLatestFrom(this.number$),
-      map(([value, todos]: any[]) => todos.concat(value))
-    ).subscribe(this.update$);
-    this.remove$.pipe(
-      withLatestFrom(this.number$),
-      map(([value, todos]: any[]) => todos.filter(v => v !== value))
-    ).subscribe(this.update$);
-  }
-
-  add() {
-    this.add$.next(this.todo);
-    this.todo = '';
-  }
-
-  remove(todo) {
-    this.remove$.next(todo);
+    this.name.setValue('Hello');
+    this.name.valueChanges.subscribe(this.nameUpdate$);
   }
 }
--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
