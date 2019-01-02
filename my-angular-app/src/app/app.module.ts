4c1ffe7e1d711d342ac2d1d55e9d1c3449e35e5a
peter.cho <peter.cho@kakaocorp.com>
2019-01-02 22:38:14 +0900

    파일 추가

--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -3,18 +3,16 @@ import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {TodoListComponent} from "./todo-list.component";
-import {TodoListService} from "./todo-list.service";
 
 @NgModule({
   declarations: [
-    AppComponent, TodoListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [TodoListService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
