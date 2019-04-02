47073367590141691914e872f24593305966de26
peter.cho <peter.cho@kakaocorp.com>
2019-04-02 20:16:21 +0900

    테스트 코드 추가

--- algorithms/test.js
+++ algorithms/test.js
@@ -9,6 +9,43 @@ const MAP = [
 
 test({
   map: MAP,
+  startPoint: [2, 3],
+  endPoint: [3, 3],
+  result: [
+    [2, 3], [3, 3]
+  ]
+});
+
+test({
+  map: MAP,
+  startPoint: [1, 2],
+  endPoint: [2, 3],
+  result: [
+    [1, 2], [1, 1], [2, 1],
+    [3, 1], [3, 2], [3, 3], [2, 3]
+  ]
+});
+
+test({
+  map: MAP,
+  startPoint: [1, 2],
+  endPoint: [0, 0],
+  result: [
+    [1, 2], [1, 1], [1, 0], [0, 0]
+  ]
+});
+
+test({
+  map: MAP,
+  startPoint: [0, 0],
+  endPoint: [1, 1],
+  result: [
+    [0, 0], [1, 0], [1, 1]
+  ]
+});
+
+test({
+  map: MAP,
   startPoint: [0, 0],
   endPoint: [3, 3],
   result: [
@@ -20,9 +57,9 @@ test({
 test({
   map: MAP,
   startPoint: [0, 0],
-  endPoint: [3, 3],
+  endPoint: [2, 3],
   result: [
     [0, 0], [1, 0], [1, 1], [2, 1],
-    [3, 1], [3, 2], [3, 3]
+    [3, 1], [3, 2], [3, 3], [2, 3]
   ]
 });
