4e95f52e1986b1edacee9c9b40c2764aca9469b2
peter.cho <peter.cho@kakaocorp.com>
2019-07-05 21:42:58 +0900

    추가

--- custom-date/custom-date.js
+++ custom-date/custom-date.js
@@ -16,4 +16,25 @@ class CustomDate extends Date {
   }
 }
 
+// addDate(count: number)
+// subtractDate(count: number)
+// format (fn) => fn({year: string, month: string, date: string, hours: string, minutes: string, seconds: string})
+// isSameDay(): boolean
+// isSameMonth(): boolean
+// isSameYear(): boolean
+// isLeapYear(): boolean
+// getDateString(): YYYY-MM-DD
+// getDateRange(date: CustomDate): number
+// getDatePeriod(date: CustomDate): number
+// getLastDay(): number
+// getYesterday(): CustomDate
+// compareYear(date: CustomDate): number
+// compareMonth(date: CustomDate): number
+// compareDate(date: CustomDate): number
+// compareHours(date: CustomDate): number
+// compareMinutes(date: CustomDate): number
+// compareSeconds(date: CustomDate): number
+// durationMonth(date: CustomDate): {month, date}
+// static create(...args): CustomDate
+
 module.exports = CustomDate
