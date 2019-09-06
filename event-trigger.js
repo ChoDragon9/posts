d06368cb65539425308a53a961300a40cf404af9
peter.cho <peter.cho@kakaocorp.com>
2019-09-06 12:30:09 +0900

    one-piece로 이동 및 삭제

--- event-trigger.js
+++
@@ -1,6 +0,0 @@
-const triggerEvent = (el, type) => {
-  const event = document.createEvent('Event')
-  event.initEvent(type, true, true)
-  el.dispatchEvent(event)
-}
-triggerEvent(document.getElementById('target'), 'touchstart')
